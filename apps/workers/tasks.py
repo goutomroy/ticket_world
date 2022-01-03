e5df084984bf276473f732b807512eef8d9e5f31
goutomroy <goutom.roy@easysolar-app.com>
2022-01-04 04:20:33 +0600

    made all celery tasks atomic

--- apps/workers/tasks.py
+++ apps/workers/tasks.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 import pytz
 from celery import shared_task
+from django.db import transaction
 from django.db.models import Q
 
 from apps.events.models import Event
@@ -10,41 +11,45 @@ from apps.reservations.models import Reservation
 
 @shared_task
 def event_starter():
-    events = Event.objects.filter(
-        status=Event.Status.CREATED, start_date__gte=datetime.now(tz=pytz.UTC)
-    )
-    objects = []
-    for event in events:
-        event.status = Event.Status.RUNNING
-        objects.append(event)
-    Event.objects.bulk_update(objects, fields=["status"])
+    with transaction.atomic():
+        events = Event.objects.filter(
+            status=Event.Status.CREATED, start_date__gte=datetime.now(tz=pytz.UTC)
+        )
+        objects = []
+        for event in events:
+            event.status = Event.Status.RUNNING
+            objects.append(event)
+        Event.objects.bulk_update(objects, fields=["status"])
 
 
 @shared_task
 def event_stopper():
-    events = Event.objects.filter(
-        status=Event.Status.RUNNING, end_date__gte=datetime.now(tz=pytz.UTC)
-    )
-    objects = []
-    for event in events:
-        event.status = Event.Status.COMPLETED
-        objects.append(event)
-    Event.objects.bulk_update(objects, fields=["status"])
+    with transaction.atomic():
+        events = Event.objects.filter(
+            status=Event.Status.RUNNING, end_date__gte=datetime.now(tz=pytz.UTC)
+        )
+        objects = []
+        for event in events:
+            event.status = Event.Status.COMPLETED
+            objects.append(event)
+        Event.objects.bulk_update(objects, fields=["status"])
 
 
 @shared_task
 def start_reservation_invalidator():
-    objects = []
-    reservations = Reservation.objects.filter(Q(status=Reservation.Status.CREATED))
-    # TODO: may be use annotation here
-    for reservation in reservations:
-        if reservation.time_elapsed_since_create() > Reservation.valid_for_seconds:
-            reservation.status = Reservation.Status.INVALIDATED
-            objects.append(reservation)
+    with transaction.atomic():
+        objects = []
+        reservations = Reservation.objects.filter(Q(status=Reservation.Status.CREATED))
+        # TODO: may be use annotation here
+        for reservation in reservations:
+            if reservation.time_elapsed_since_create() > Reservation.valid_for_seconds:
+                reservation.status = Reservation.Status.INVALIDATED
+                objects.append(reservation)
 
-    Reservation.objects.bulk_update(objects, fields=["status"])
+        Reservation.objects.bulk_update(objects, fields=["status"])
 
 
 @shared_task
 def make_refund(payment_id):
-    pass
+    with transaction.atomic():
+        pass
